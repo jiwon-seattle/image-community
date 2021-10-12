dda3c9f6220489688c96c9c487f0e4cc503c7c47
Jiwon Han <42082640+jiwon-seattle@users.noreply.github.com>
2021-10-11 21:38:22 -0700

    cookie and login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,13 @@
 import React from "react";
 import {Text, Input, Grid, Button} from "../elements"
+import { getCookie, setCookie, deleteCookie } from "../shared/Cookie";
 
 const Login = (props) => {
-
+  console.log(getCookie('user_id'));
+  const login = () => {
+    setCookie("user_id", 'jiwon', 3);
+    setCookie("user_pwd", 'jiwon7', 3);
+  }
   return (
     <React.Fragment>
       <Grid padding="16px">
@@ -13,7 +18,7 @@ const Login = (props) => {
         <Grid padding="16px 0px">
           <Input label="Password" placeholder="Type Password" _onChange={()=>{console.log('Password has been typed!')}}/>
         </Grid>
-        <Button text="LogIn" _onClick={() => {console.log('Button Clicked')}}></Button>
+        <Button text="LogIn" _onClick={() => {login(); deleteCookie("user_id");}}></Button>
       </Grid>
     </React.Fragment>
   )
